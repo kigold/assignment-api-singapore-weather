f607e7a0270bb677fab245caff2b5817f11dd320
kigold <kinglseybox@yahoo.com>
2016-07-08 12:18:42 +0100

    2hr Nowcast dataset working

--- api.js
+++ api.js
@@ -1,11 +1,49 @@
 $(document)
 	.ready(function() {
-
+	//variables declareation
 	var key = '&keyref=' + '781CF461BB6606ADEA01E0CAF8B35274746BD40916748CE0';
 	var dataset = '2hr_nowcast'
 	var baseurl = 'http://www.nea.gov.sg/api/WebAPI?dataset='
 	var url = baseurl + dataset  + key;
+	var api_data = {};
 
+	//Weather Forecast Abbreviation Interpretation List
+	var forecast_interpretation = {
+		'BR' : 'Mist',
+		'CL' : 'Cloudy',
+		'DR' : 'Drizzle',
+		'FA' : 'Fair (Day)',
+		'FG' : 'Fog',
+		'FN' : 'Fair (Night)',
+		'FW' : 'Fair & Warm',
+		'HG' : 'Heavy Thundery Showers with Gusty Winds',
+		'HR' : 'Heavy Rain',
+		'HS' : 'Heavy Showers',
+		'HT' : 'Heavy Thundery Shower',
+		'HZ' : 'Hazy',
+		'LH' : 'Slightly Hazy',
+		'LR' : 'Light Rain',
+		'LS' : 'Light Showers',
+		'OC' : 'Overcast',
+		'PC' : 'Partly Cloudy(Day)',
+		'PN' : 'Partly Cloudy(Night)',
+		'PS' : 'Passing Showers',
+		'RA' : 'Moderate Rain',
+		'SH' : 'Showers',
+		'SK' : 'Strong Winds, Showers',
+		'SN' : 'Snow',
+		'SR' : 'Strong Winds, Rain',
+		'SS' : 'Snow Showers',
+		'SU' : 'Sunny',
+		'SW' : 'Strong Winds',
+		'TL' : 'Thundery Showers',
+		'WC' : 'Windy, Cloudy',
+		'WD' : 'Windy',
+		'WF' :'Windy, Fair',
+		'WR' : 'Windy, Rain',
+		'WS' : 'Windy, Showers',
+	//end forecast_interpretation
+	};
 
 	// Changes XML to JSON
 	function xmlToJson(xml) {
@@ -44,131 +82,124 @@ $(document)
 			}
 		}
 		return obj;
-
+	//end xmlToJson
 	};
-	////////////
 
-
-	///Weather Forecast Abbreviation Interpretation List
-	var forecast_interpretation = {
-		'BR' : 'Mist',
-		'CL' : 'Cloudy',
-		'DR' : 'Drizzle',
-		'FA' : 'Fair (Day)',
-		'FG' : 'Fog',
-		'FN' : 'Fair (Night)',
-		'FW' : 'Fair & Warm',
-		'HG' : 'Heavy Thundery Showers with Gusty Winds',
-		'HR' : 'Heavy Rain',
-		'HS' : 'Heavy Showers',
-		'HT' : 'Heavy Thundery Shower',
-		'HZ' : 'Hazy',
-		'LH' : 'Slightly Hazy',
-		'LR' : 'Light Rain',
-		'LS' : 'Light Showers',
-		'OC' : 'Overcast',
-		'PC' : 'Partly Cloudy(Day)',
-		'PN' : 'Partly Cloudy(Night)',
-		'PS' : 'Passing Showers',
-		'RA' : 'Moderate Rain',
-		'SH' : 'Showers',
-		'SK' : 'Strong Winds, Showers',
-		'SN' : 'Snow',
-		'SR' : 'Strong Winds, Rain',
-		'SS' : 'Snow Showers',
-		'SU' : 'Sunny',
-		'SW' : 'Strong Winds',
-		'TL' : 'Thundery Showers',
-		'WC' : 'Windy, Cloudy',
-		'WD' : 'Windy',
-		'WF' :'Windy, Fair',
-		'WR' : 'Windy, Rain',
-		'WS' : 'Windy, Showers',
-	}
-
-	$.ajax({
-	  url: url,
-	  type: 'GET',
-	  success: function(data){
-	  		var result = xmlToJson(data);
-		
-
-			///////////After Call	
-			$('#show').html(result['channel']['description']['#text']);
-			$('#title').html(result['channel']['item']['title']['#text']);
-			$('#time').html(result['channel']['item']['validTime']['#text']);
-			$('#source').html(result['channel']['source']['#text']);
-			var length = result['channel']['item']['weatherForecast']['area'].length;
-			//Create List of cites
-			var cities = [];
-			for(var i=0; i<length; i++){
-				cities.push(result['channel']['item']['weatherForecast']['area'][i]['@attributes']['name']);
-				$('#cities').append('<option value='+ cities[i] + '>' + cities[i] + '</option>');
-			}
-
-
-
-			//List All cites and Forcast 
-			for(var i=0; i<length; i++){
-				//$('#loc').append(result['channel']['item']['weatherForecast']['area'][i]['@attributes']['name'] + '<br/>');
-				//$('#fc').append(result['channel']['item']['weatherForecast']['area'][i]['@attributes']['forecast']+ '<br/>');
-				var location = result['channel']['item']['weatherForecast']['area'][i]['@attributes']['name'];
-				var forecast = result['channel']['item']['weatherForecast']['area'][i]['@attributes']['forecast'];
-				$('tbody').append('<tr><td>' + location +  '</td><td>' + forecast_interpretation[forecast] + '</td></tr>')
-
-			}
-		}
-
-	});
-	//weatherApi takes url, makes th API and returns the data
-	var url = baseurl + dataset  + key;
-	function WeatherApi(baseurl, dataset, key) {
-		this.data = null;
+	var WeatherApi = function (baseurl, key) {
+		//class to access weather API
+		//initialisation
 		this.baseurl = baseurl;
 		this.key = key;
-		this.dataset = dataset
-		//api call
-		this.callApi = function(dataset) {
-			if (dataset) {
-				url = this.baseurl + dataset + this.key;
+		this.dataset = 'not set';
+		this.data = {};
+
+		// callback functionto setData
+		function setData(data){
+			this.data = xmlToJson(data);
+			api_data = this.data;
+		//end setDate
+		};
+
+		// populate page with api query result
+		this.populate = function(dataset, city){
+			var city_index = '', cities = [];
+			var length = this.data['channel']['item']['weatherForecast']['area'].length;
+			if (dataset ==='2hr_nowcast'){
+				for (var i=0; i < length; i++){
+					//get dataset response for city
+					if (this.data['channel']['item']['weatherForecast']['area'][i]['@attributes']['name'] == city){city_index=i; break;}
+				}
+				var location = this.data['channel']['item']['weatherForecast']['area'][city_index]['@attributes']['name'];
+				var forecast = this.data['channel']['item']['weatherForecast']['area'][city_index]['@attributes']['forecast'];
+				$('#table_content').html('<tr><td>' + location +  '</td><td>' + forecast_interpretation[forecast] + '</td></tr>');
+				//List All cites and Forcast 
+				for(var i=0; i< length; i++){
+					var location = this.data['channel']['item']['weatherForecast']['area'][i]['@attributes']['name'];
+					var forecast = this.data['channel']['item']['weatherForecast']['area'][i]['@attributes']['forecast'];
+					$('#table_content').append('<tr><td>' + location +  '</td><td>' + forecast_interpretation[forecast] + '</td></tr>')
+
+				}
 			}
+			else if ( dataset === '24hrs_forecast')
+			{
+
+			}
+
 			else {
-				url = this.baseurl + this.dataset + this.key;
+				var location = 'NULL ' + dataset;
+				var forecast = 'NULL ' + dataset;
+				$('#table_content').html('<tr><td>' + location +  '</td><td>' + forecast_interpretation[forecast] + '</td></tr>');
 			}
-			$.get(url, function(data){
-				this.data = xmlToJson(data);
-				console.log('weatherApi class Ran successfully');
-				console.log(data);
+			// List all cities in drop down of id cities
+			for(var i=0; i<length; i++){
+				cities.push(this.data['channel']['item']['weatherForecast']['area'][i]['@attributes']['name']);
+				$('#cities').append('<option value="'+ cities[i] + '">' + cities[i] + '</option>');
+			}
+		//end populate
+		};
+
+		//get data from api
+		this.callApi = function(dataset, city, populate){
+			var url = this.baseurl + dataset + this.key;
+			//ajax call of api
+			$.ajax({
+				url:url,
+				success:function(data){
+					setData(data);
+					populate(dataset,city);
+				}
 			});
-		}
-		//populate table
-		this.populate = function(dataset,city){
-			this.callApi(dataset);
-			if (dataset='2hr_nowcast'){
-				var location = this.data['channel']['item']['weatherForecast']['area'][i]['@attributes']['name'];
-				var forecast = this.data['channel']['item']['weatherForecast']['area'][i]['@attributes']['forecast'];
-				$('tbody').html('<tr><td>' + location +  '</td><td>' + forecast_interpretation[forecast] + '</td></tr>');
+		//end callApi
 
+		};
+	//end WeatherApi class
+	};
+	/////////#####TESTING API
+	var weatherApi = new WeatherApi(baseurl, key);
+	var w = weatherApi;
+	w.callApi('2hr_nowcast', 'Changi', w.populate);
+
+	function populate(dataset, city) {
+		var city_index = '';
+		
+		if (dataset ==='2hr_nowcast'){
+			var length = api_data['channel']['item']['weatherForecast']['area'].length;
+			for (var i=0; i < length; i++){
+				//get dataset response for city
+				if (api_data['channel']['item']['weatherForecast']['area'][i]['@attributes']['name'] === city){city_index=i; break;}
 			}
-			else {
-
-			}
+			var location = api_data['channel']['item']['weatherForecast']['area'][city_index]['@attributes']['name'];
+			var forecast = api_data['channel']['item']['weatherForecast']['area'][city_index]['@attributes']['forecast'];
+			$('#table_content').html('<tr><td>' + location +  '</td><td>' + forecast_interpretation[forecast] + '</td></tr>');
+			 
+		}
+		else if ( dataset === '24hrs_forecast')
+		{
 
 		}
 
+		else {
+			var location = 'NULL ' + dataset;
+			var forecast = 'NULL ' + dataset;
+			$('#table_content').html('<tr><td>' + location +  '</td><td>' + forecast_interpretation[forecast] + '</td></tr>');
+		}
+	//end populate
+	};
 
-	}
-
-	var weatherApi = new WeatherApi(baseurl, dataset, key);
-	weatherApi.callApi('24hrs_forecast');
-
-
-
-
-
-
-
-	///////////
+	////show results on page
 	
-});
+	$('#cities').on('change', function (){
+		populate(dataset, $('#cities').val());
+		console.log(api_data);
+	});
+	
+	$('#data_set').on('change', function (){
+		w.callApi($('#data_set').val(), $('#cities').val(), populate);
+		console.log(api_data)
+	});
+	//end show results on page
 
+	////////#####end TESTING API
+
+//end document
+	});
